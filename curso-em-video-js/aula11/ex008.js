b87f5c4b1a657376c49a2e49ab93ea787150fba0
Lilian Bernardo dos Santos <lilianprodev@gmail.com>
2024-10-21 09:52:09 -0300

    Create ex008.js

---
+++ curso-em-video-js/aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 30
+console.log(`A velocidade é de ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Voçê ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
