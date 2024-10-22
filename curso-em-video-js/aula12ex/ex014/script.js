d1b81b400a869795344c2c37e33cc417199c00ff
Lilian Bernardo dos Santos <lilianprodev@gmail.com>
2024-10-22 15:00:54 -0300

    atualizado

--- curso-em-video-js/aula12ex/ex014/script.js
+++ curso-em-video-js/aula12ex/ex014/script.js
@@ -1,2 +1,31 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 5
+    msg.innerHTML = `Agora são ${hora} horas.`
+
+    
+
+   
+
+    if (hora >= 0 && hora < 12) {
+
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#fec100'
+
+    } else if (hora >= 12 && hora < 18) {
+
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#ff7300'
+    } else {
+
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#000813'
+    }
+
+}
+
+
 
