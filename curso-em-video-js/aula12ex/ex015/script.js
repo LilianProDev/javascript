97425e8e2cb466ecf89d4c5652e7beb4a3b62ab1
Lilian Bernardo dos Santos <lilianprodev@gmail.com>
2024-10-22 16:14:36 -0300

    atualizando

--- curso-em-video-js/aula12ex/ex015/script.js
+++ curso-em-video-js/aula12ex/ex015/script.js
@@ -1,2 +1,18 @@
-alert('Olá')
+function verificar() {
+    var ano = window.document.getElementById('anonasc')
+    var sexom = window.document.getElementById('masc')
+    var sexof = window.document.getElementById('fem')
+    
+    ano.innerHTML = `Detectamos ${sexo} com ano`
+
+    if ( > 18 ) {
+        img.src = imagens/masculino.png
+    }
+
+
+
+
+}
+
+
 
