86dfa04fd5b1ae0b03bcd3ba17c8d9e4d8bad746
Lilian Bernardo dos Santos <lilianprodev@gmail.com>
2024-11-21 07:44:29 -0300

    atualizado

--- exercicios/praticando/ex001/node_4474cf3308834.tmp.js
+++ exercicios/praticando/ex001/node_4474cf3308834.tmp.js
@@ -8,12 +8,14 @@
 } */
 
 let somar = 0
-for (let soma = 1; soma <= 100; soma++) {
+for (let soma = 1; soma <= 100; soma--) {
 
     somar += soma
 
     console.log("A soma de: " + somar  + " + " + soma + "=" + soma)
-} 
+}  
+
  
+
     
     
