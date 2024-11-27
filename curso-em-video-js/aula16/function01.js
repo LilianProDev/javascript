d1afe8144d8a931c73077abf8418e06d21d2b079
Lilian Bernardo dos Santos <lilianprodev@gmail.com>
2024-11-27 08:38:08 -0300

    Create function01.js

---
+++ curso-em-video-js/aula16/function01.js
@@ -0,0 +1,11 @@
+function parimp(n) {
+    if (n%2 === 0 ) {
+        return 'Par'
+
+    } else {
+        return 'Ìmpar'
+    }       
+}
+
+let res = parimp(4)
+console.log('O número é: ' + res )
